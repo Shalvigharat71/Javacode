b6249dc2b03a7838d244cf07040836ce3d6bda76
ADMIN <ADMIN@ADMIN-PC>
2020-09-11 13:20:19 +0530

    Assignments

---
+++ JavaProject2/src/Iteration3.java
@@ -0,0 +1,17 @@
+
+public class Iteration3 
+{
+	public static void main(String args[])
+	{
+                for(char ch='A';ch<='F';ch++)            	
+                {
+                	for(char j='A';j<=ch;j++)        
+		            {
+                		System.out.print(j + " "); 
+		            }
+		            System.out.println();        
+                }	
+	}
+
+
+} 
